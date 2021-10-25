271e2fd8058263d35b90ccb748f3bce4372db535
Nikhileshwar <nikhileshwar.vijayakumar@syncfusion.com>
2021-10-25 16:05:05 +0530

    UI glitch

--- lib/pages/movies_browserer_page.dart
+++ lib/pages/movies_browserer_page.dart
@@ -37,186 +37,189 @@ class _MovieBrowsePageState extends State<MovieBrowsePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    'Browse',
-                    style: Theme.of(context).textTheme.headline4?.copyWith(
-                          fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Browse',
+                      style: Theme.of(context).textTheme.headline4?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isFilterOpen = !isFilterOpen;
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.filter_list,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              isFilterOpen
+                  ? Column(
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: seaarchType == 'All',
+                              onChanged: (currentState) {
+                                setState(() {
+                                  if (currentState ?? false) {
+                                    seaarchType = 'All';
+                                  }
+
+                                  BlocProvider.of<MovieBrowseBloc>(context)
+                                      .add(FilterContent(ContentType.all));
+                                });
+                              },
+                            ),
+                            const Expanded(
+                              child: Text('All'),
+                            ),
+                          ],
                         ),
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: seaarchType == 'Movies',
+                              onChanged: (currentState) {
+                                setState(() {
+                                  if (currentState ?? false) {
+                                    seaarchType = 'Movies';
+                                  }
+
+                                  BlocProvider.of<MovieBrowseBloc>(context)
+                                      .add(FilterContent(ContentType.movies));
+                                });
+                              },
+                            ),
+                            const Expanded(
+                              child: Text('Movies'),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: seaarchType == 'Series',
+                              onChanged: (currentState) {
+                                setState(() {
+                                  if (currentState ?? false) {
+                                    seaarchType = 'Series';
+                                  }
+
+                                  BlocProvider.of<MovieBrowseBloc>(context)
+                                      .add(FilterContent(ContentType.series));
+                                });
+                              },
+                            ),
+                            const Expanded(
+                              child: Text('Series'),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  : Text(
+                      seaarchType,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: const Color(0xFF333333),
+                child: TextField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    hintText: 'Search',
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    hintStyle: Theme.of(context).textTheme.bodyText2,
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isFilterOpen = !isFilterOpen;
-                    });
+                  controller: searchController,
+                  onSubmitted: (value) {
+                    BlocProvider.of<MovieBrowseBloc>(context)
+                        .add(SearchMovies(searchController.text));
                   },
-                  icon: const Icon(
-                    Icons.filter_list,
-                  ),
+                  onChanged: (value) {
+                    BlocProvider.of<MovieBrowseBloc>(context)
+                        .add(SearchMovies(searchController.text));
+                  },
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            isFilterOpen
-                ? Column(
-                    children: [
-                      Row(
-                        children: [
-                          Checkbox(
-                            value: seaarchType == 'All',
-                            onChanged: (currentState) {
-                              setState(() {
-                                if (currentState ?? false) {
-                                  seaarchType = 'All';
-                                }
+              ),
+              Expanded(
+                child: BlocBuilder<MovieBrowseBloc, MovieBrowseState>(
+                  builder: (context, state) {
+                    if (state is MovieListLoaded) {
+                      if (state.movies.isEmpty) {
+                        return const NoSearchMade();
+                      }
+                      return GridView.builder(
+                        padding: const EdgeInsets.all(
+                          20,
+                        ),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          childAspectRatio:
+                              (MediaQuery.of(context).size.width / 3) / 300,
+                          mainAxisSpacing: 20,
+                          crossAxisSpacing: 20,
+                        ),
+                        itemCount: state.movies.length,
+                        itemBuilder: (itemContext, index) =>
+                            MovieItemView(state.movies[index]),
+                      );
+                    }
 
-                                BlocProvider.of<MovieBrowseBloc>(context)
-                                    .add(FilterContent(ContentType.all));
-                              });
-                            },
-                          ),
-                          const Expanded(
-                            child: Text('All'),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Checkbox(
-                            value: seaarchType == 'Movies',
-                            onChanged: (currentState) {
-                              setState(() {
-                                if (currentState ?? false) {
-                                  seaarchType = 'Movies';
-                                }
+                    if (state is MovieListLoading) {
+                      return const MovieSearchLoading();
+                    }
 
-                                BlocProvider.of<MovieBrowseBloc>(context)
-                                    .add(FilterContent(ContentType.movies));
-                              });
-                            },
-                          ),
-                          const Expanded(
-                            child: Text('Movies'),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Checkbox(
-                            value: seaarchType == 'Series',
-                            onChanged: (currentState) {
-                              setState(() {
-                                if (currentState ?? false) {
-                                  seaarchType = 'Series';
-                                }
+                    if (state is MovieListFailed) {
+                      switch (state.failureType) {
+                        case SearchFailureType.unknown:
+                          return searchController.text.isNotEmpty
+                              ? const MovieSearchFailed()
+                              : const MovieSearchTooManyResults();
+                        case SearchFailureType.movieNotFound:
+                          return const MovieNotFound();
+                        case SearchFailureType.toomanyResults:
+                          return const MovieSearchTooManyResults();
+                      }
+                    }
 
-                                BlocProvider.of<MovieBrowseBloc>(context)
-                                    .add(FilterContent(ContentType.series));
-                              });
-                            },
-                          ),
-                          const Expanded(
-                            child: Text('Series'),
-                          ),
-                        ],
-                      )
-                    ],
-                  )
-                : Text(
-                    seaarchType,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              color: const Color(0xFF333333),
-              child: TextField(
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                  hintStyle: Theme.of(context).textTheme.bodyText2,
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                    ),
-                  ),
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                    ),
-                  ),
+                    return Container();
+                  },
                 ),
-                controller: searchController,
-                onSubmitted: (value) {
-                  BlocProvider.of<MovieBrowseBloc>(context)
-                      .add(SearchMovies(searchController.text));
-                },
-                onChanged: (value) {
-                  BlocProvider.of<MovieBrowseBloc>(context)
-                      .add(SearchMovies(searchController.text));
-                },
               ),
-            ),
-            Expanded(
-              child: BlocBuilder<MovieBrowseBloc, MovieBrowseState>(
-                builder: (context, state) {
-                  if (state is MovieListLoaded) {
-                    if (state.movies.isEmpty) {
-                      return const NoSearchMade();
-                    }
-                    return GridView.builder(
-                      padding: const EdgeInsets.all(
-                        20,
-                      ),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        childAspectRatio:
-                            (MediaQuery.of(context).size.width / 3) / 300,
-                        mainAxisSpacing: 20,
-                        crossAxisSpacing: 20,
-                      ),
-                      itemCount: state.movies.length,
-                      itemBuilder: (itemContext, index) =>
-                          MovieItemView(state.movies[index]),
-                    );
-                  }
-
-                  if (state is MovieListLoading) {
-                    return const MovieSearchLoading();
-                  }
-
-                  if (state is MovieListFailed) {
-                    switch (state.failureType) {
-                      case SearchFailureType.unknown:
-                        return searchController.text.isNotEmpty
-                            ? const MovieSearchFailed()
-                            : const MovieSearchTooManyResults();
-                      case SearchFailureType.movieNotFound:
-                        return const MovieNotFound();
-                      case SearchFailureType.toomanyResults:
-                        return const MovieSearchTooManyResults();
-                    }
-                  }
-
-                  return Container();
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
